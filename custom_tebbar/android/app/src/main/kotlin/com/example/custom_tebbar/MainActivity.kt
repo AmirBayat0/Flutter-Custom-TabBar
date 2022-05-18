16bdb8dc5eca6e0361e6f4d8b8aef61fa3db18c2
AmirBayat0 <Amirbayat.dev@gmail.com>
2022-05-18 20:38:22 +0430

    Files & Codes Added

---
+++ custom_tebbar/android/app/src/main/kotlin/com/example/custom_tebbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.custom_tebbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ custom_tebbar/lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainHome(),
+    );
+  }
+}
+
+class MainHome extends StatefulWidget {
+  const MainHome({Key? key}) : super(key: key);
+
+  @override
+  State<MainHome> createState() => _MainHomeState();
+}
+
+class _MainHomeState extends State<MainHome> {
+  /// List of Tab Bar Item
+  List<String> items = [
+    "Home",
+    "Explore",
+    "Search",
+    "Feed",
+    "Post",
+    "Activity",
+    "Setting",
+    "Profile",
+  ];
+
+  /// List of body icon
+  List<IconData> icons = [
+    Icons.home,
+    Icons.explore,
+    Icons.search,
+    Icons.feed,
+    Icons.post_add,
+    Icons.local_activity,
+    Icons.settings,
+    Icons.person
+  ];
+  int current = 0;
+
+  /////////////////////////////////////
+  //@CodeWithFlexz on Instagram
+  //
+  //AmirBayat0 on Github
+  //Programming with Flexz on Youtube
+  /////////////////////////////////////
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+
+      /// APPBAR
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text(
+          "Custom TabBar",
+          style: GoogleFonts.laila(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        margin: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            /// CUSTOM TABBAR
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: items.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (ctx, index) {
+                    return Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              current = index;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            margin: const EdgeInsets.all(5),
+                            width: 80,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              color: current == index
+                                  ? Colors.white70
+                                  : Colors.white54,
+                              borderRadius: current == index
+                                  ? BorderRadius.circular(15)
+                                  : BorderRadius.circular(10),
+                              border: current == index
+                                  ? Border.all(
+                                      color: Colors.deepPurpleAccent, width: 2)
+                                  : null,
+                            ),
+                            child: Center(
+                              child: Text(
+                                items[index],
+                                style: GoogleFonts.laila(
+                                    fontWeight: FontWeight.w500,
+                                    color: current == index
+                                        ? Colors.black
+                                        : Colors.grey),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Visibility(
+                            visible: current == index,
+                            child: Container(
+                              width: 5,
+                              height: 5,
+                              decoration: const BoxDecoration(
+                                  color: Colors.deepPurpleAccent,
+                                  shape: BoxShape.circle),
+                            ))
+                      ],
+                    );
+                  }),
+            ),
+
+            /// MAIN BODY
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              width: double.infinity,
+              height: 550,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    icons[current],
+                    size: 200,
+                    color: Colors.deepPurple,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    items[current],
+                    style: GoogleFonts.laila(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30,
+                        color: Colors.deepPurple),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
